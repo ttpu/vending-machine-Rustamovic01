1841a137b3ca4d5f209854718f1044950b535e7a
Rustamovic01 <bekzodmuqimovcse@gmail.com>
2025-05-24 00:26:49 +0500

    Update app.java

--- src/app.java
+++ src/app.java
@@ -1,32 +1,26 @@
-public class app {
+public class App {
     public static void main(String[] args) {
-
         VendingMachine vm = new VendingMachine();
 
-        vm.addBeverage("Coke", 0.50);
-        vm.addBeverage("Water", 0.30);
+        vm.addBeverage("tosh", 0.50);
+        vm.addBeverage("atir", 0.30);
         vm.addBeverage("Beer", 1.00);
-
-        vm.refillColumn(1, "Coke", 1);
-        vm.refillColumn(2, "Beer", 10);
-        vm.refillColumn(3, "Coke", 15);
-        vm.refillColumn(4, "Water", 20);
-
-        System.out.println(vm.availableCans("Coke"));
-
-        System.out.println(vm.getPrice("Coke"));
 
         vm.rechargeCard(12, 5.5);
         vm.rechargeCard(21, 10.0);
         vm.rechargeCard(99, 0.75);
 
-        vm.sell("Coke", 12);
-        vm.sell("Coke", 99);
-        vm.sell("Water", 12);
+        vm.refillColumn(1, "tosh", 1);
+        vm.refillColumn(2, "Beer", 10);
+        vm.refillColumn(3, "tosh", 15);
+        vm.refillColumn(4, "atir", 20);
 
-        System.out.println(vm.availableCans("Coke"));
-
+        System.out.println(vm.getPrice("tosh"));
         System.out.println(vm.getCredit(12));
+        System.out.println(vm.availableCans("tosh"));
 
+        int result = vm.sell("tosh", 12);
+        System.out.println(result);
+        System.out.println(vm.getCredit(12));
     }
 }
